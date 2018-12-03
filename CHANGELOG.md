755a56b6a0b9e2774979ee892d322b4ff2734e44
Kornél Horváth <kornel.horvath@me.com>
2018-12-03 09:18:24 +0100

    Changed: ignoreWhenBindingPresent set to true in no-unused-components rule

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Changed
+
+- ignoreWhenBindingPresent set to true in no-unused-components rule
+
 
 ## [2.0.2] - 2018-11-26
 
--- index.js
+++ index.js
@@ -77,7 +77,7 @@ module.exports = {
 		'vue/no-template-key': 'error',
 		'vue/no-template-shadow': 'error',
 		'vue/no-textarea-mustache': 'error',
-		'vue/no-unused-components': 'error',
+		'vue/no-unused-components': [ 'error', { 'ignoreWhenBindingPresent': true } ],
 		'vue/no-unused-vars': 'error',
 		'vue/no-use-v-if-with-v-for': [ 'error', { 'allowUsingIterationVar': false } ],
 		'vue/no-v-html': 'error',
