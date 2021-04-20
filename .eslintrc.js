e6db1332010f585aec38537bd06e54e7a345886a
Kornél Horváth <kornel.horvath@me.com>
2021-04-20 18:51:32 +0200

    Updated project files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
 	root: true,
 	extends: '@koffeine',
+	parserOptions: {
+		ecmaVersion: 2021, // 12
+		sourceType: 'script'
+	},
 	env: {
+		es2021: true,
 		node: true
 	},
 	rules: {
